99fa3b05668f6bd17e87882377ae97b0836dc6ef
wanderkpot <wanderkpot@gmail.com>
2016-08-02 19:46:33 -0300

    senaiSucks

--- src/stock/JDBC/JDBCTransactionDAO.java
+++ src/stock/JDBC/JDBCTransactionDAO.java
@@ -18,7 +18,7 @@ public class JDBCTransactionDAO {
 		stbd.append("INSERT INTO transactions (");
 		stbd.append("productId, manufacturer, quantity, isEntry, expectedDate, deliveryDate, outputDate");
 		stbd.append(") VALUES (?,?,?,?,?,?,?)");
-		
+
 		PreparedStatement p;
 
 		try {
@@ -31,9 +31,45 @@ public class JDBCTransactionDAO {
 			p.setString(6, tst.getDeliveryDate());
 			p.setString(7, tst.getOutputDate());
 			p.execute();
+
+			insertIntoStock(tst.getQuantity(), tst.getProductId());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	private void insertIntoStock(int quantity, int productId) {
+		StringBuilder stbd = new StringBuilder();
+
+		stbd.append(existInStock(productId) ? "" : "");
+
+		PreparedStatement p;
+
+		try {
+			p = this.connection.prepareStatement(stbd.toString());
+			p.setInt(1, quantity);
+			p.setInt(2, productId);
+			p.execute();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private boolean existInStock(int productId) {
+		StringBuilder stbd = new StringBuilder();
+
+		stbd.append("SELECT quantity FROM stock WHERE productId = ?");
+
+		PreparedStatement p;
+
+		try {
+			p = this.connection.prepareStatement(stbd.toString());
+			p.setInt(1, productId);
+			p.execute();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
 	}
 
 }
